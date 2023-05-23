34013a9b89cb46931e117b11e8ca8caffc073431
Qtester94 <qtestr94@gmail.com>
2023-05-23 10:28:50 +0200

    Thusday

---
+++ Test Suites/Box/S23 Login0523A.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>S23 Login0523A</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>873a5ab4-d1e4-44b0-af6a-460f9ac8fe91</testSuiteGuid>
+   <testCaseLink>
+      <guid>695a1062-417b-4f6e-a33f-b8885abe6f8c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BOX/Test Login with valid credentials</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>102c8b13-aceb-421c-9545-4b9b4487c694</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>13174997-850d-459f-9737-6ff54910f0ad</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
